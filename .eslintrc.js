54c582ed4437718acfc611e7f290682c9c4c2f17
Pedro Pizzo de Paula <poloo.p3@gmail.com>
2020-03-06 22:47:42 -0300

    configs / folders / lint and redux bases

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,39 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  env: {
+    es6: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier', 'prettier/react'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    __DEV__: true,
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier', 'react-hooks'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [1, {extensions: ['.js', '.jsx']}],
+    'import/prefer-default-export': 'off',
+    'react/state-in-constructor': [1, 'never'],
+    'no-param-reassign': 'off',
+    'no-console': ['error', {allow: ['tron']}],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/jsx-props-no-spreading': 'off',
+    'no-underscore-dangle': ['error', {allow: ['_id']}],
+  },
+  settings: {
+    'import/resolver': {
+      'babel-plugin-root-import': {
+        rootPathSuffix: 'src',
+      },
+    },
+  },
 };
